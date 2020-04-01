0d50657f794e901a530bbf5d53b9bfc665770921
flmngwllm <flmngwllm232@gmail.com>
2020-04-01 17:19:09 -0400

    created constructors in budget controller for expenses and income

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 // Budger controller
 var budgetController = (function() {
     
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    }
 
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value; 
+    }
+    
 })()
 
 //UI controller
