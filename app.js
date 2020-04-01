b5208f2c6ae845ddd582a28b4ba0fdc7f4390545
flmngwllm <flmngwllm232@gmail.com>
2020-04-01 16:30:20 -0400

    wrote code in UI controller that gets the value for type, description and value and passes it to the main controller

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-
+// Budger controller
 var budgetController = (function() {
     
 
 })()
 
-
+//UI controller
 var UIController = (function() {
+
+    var DOMstring = {
+        inputType: 'add__type',
+        inputeDescription: 'add__description',
+        inputValue: 'add__value',
+        inputBtn: 'add__button'
+    }
+    return {
+        getInput: function(){
+            return {
+                type : document.querySelector(inputType).value, // will be either inc or exp
+                description : document.querySelector(inputeDescription).value,
+                value :document.querySelector(inputValue).value
+            }
+        },
+
+        getDOMstrings: function(){
+            return DOMstring;
+        }
+
+    }
 
 })();
 
 
-
+//Main controller
 var controller = (function(budgetContr, UIContr) {
+
+    var DOM = UIContr.getDOMstrings()
 
     var contrAddItem = function(){
         // Get input data
-
+        var input = UIContr.getInput();
+        console.log(input);
+        
         // Add item to budget controller
 
         // Add item to UI
@@ -25,7 +50,7 @@ var controller = (function(budgetContr, UIContr) {
         // Display budget on the UI
     }
 
-document.querySelector(".add__button").addEventListener('click', contrAddItem)
+document.querySelector(DOM.inputBtn).addEventListener('click', contrAddItem)
 
 
 // event listener for enter key
