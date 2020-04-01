01b1e1620df343a34361bbeaafbfed418692d0ba
flmngwllm <flmngwllm232@gmail.com>
2020-04-01 15:51:58 -0400

    created click eventListener for addbutton and when someone presses the enter key in controller function

--- app.js
+++ app.js
@@ -13,5 +13,26 @@ var UIController = (function() {
 
 var controller = (function(budgetContr, UIContr) {
 
+    var contrAddItem = function(){
+        // Get input data
+
+        // Add item to budget controller
+
+        // Add item to UI
+
+        // Calculate the budget
+
+        // Display budget on the UI
+    }
+
+document.querySelector(".add__button").addEventListener('click', contrAddItem)
+
+
+// event listener for enter key
+document.addEventListener('keypress', function(event){
+    if('keypress' === 13 || event.which === 13) {
+        contrAddItem()
+    }
+})
    
 })(budgetController, UIController);
