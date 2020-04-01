5e270803739ba5dc56a93e9c4a9e00fe28e316d6
flmngwllm <flmngwllm232@gmail.com>
2020-04-01 17:12:32 -0400

    fixed a spelling error in UI controller and created an init function in manin controller

--- app.js
+++ app.js
@@ -8,17 +8,18 @@ var budgetController = (function() {
 var UIController = (function() {
 
     var DOMstring = {
-        inputType: 'add__type',
-        inputeDescription: 'add__description',
-        inputValue: 'add__value',
-        inputBtn: 'add__button'
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__button'
     }
+
     return {
         getInput: function(){
             return {
-                type : document.querySelector(inputType).value, // will be either inc or exp
-                description : document.querySelector(inputeDescription).value,
-                value :document.querySelector(inputValue).value
+                type : document.querySelector(DOMstring.inputType).value, // will be either inc or exp
+                description : document.querySelector(DOMstring.inputDescription).value,
+                value : document.querySelector(DOMstring.inputValue).value
             }
         },
 
@@ -34,13 +35,26 @@ var UIController = (function() {
 //Main controller
 var controller = (function(budgetContr, UIContr) {
 
-    var DOM = UIContr.getDOMstrings()
+    var setupEventListeners = function()  {
+        
+        var DOM = UIContr.getDOMstrings()
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', contrAddItem);
+
+        // event listener for enter key
+        document.addEventListener('keypress', function(event){
+        if('keypress' === 13 || event.which === 13) {
+        contrAddItem()
+        }
+    })
+}
+
 
     var contrAddItem = function(){
         // Get input data
         var input = UIContr.getInput();
         console.log(input);
-        
+
         // Add item to budget controller
 
         // Add item to UI
@@ -50,14 +64,14 @@ var controller = (function(budgetContr, UIContr) {
         // Display budget on the UI
     }
 
-document.querySelector(DOM.inputBtn).addEventListener('click', contrAddItem)
-
-
-// event listener for enter key
-document.addEventListener('keypress', function(event){
-    if('keypress' === 13 || event.which === 13) {
-        contrAddItem()
-    }
-})
+    return {
+        init: function() {
+            console.log("app started")
+            setupEventListeners()
+        }
+    };
    
 })(budgetController, UIController);
+
+
+controller.init()
